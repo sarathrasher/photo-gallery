10d6584f9e027af2ae5c13169f6bae8243235bbf
Sara Muntean <munteansr@gmail.com>
2018-07-24 08:51:29 -0400

    Added more posts and made the hover image feature fully functional. Added initial steps for a modal.

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 var images = [
-    {caption: 'Sometimes life can get messy.', url: "images/messy-desk.png"},
+    {caption: 'Quilting pattern idea for my plus quilt.', url: "images/plus-quilting-pattern.jpg"},
     {caption: 'Initial layout for my plus quilt.', url: "images/plus-quilt.png"},
+    {caption: 'Sometimes life can get messy.', url: "images/messy-desk.png"},
     {caption: 'Rare moment--a clean desk.', url: "images/ordered-desk.png"},
+    {caption: 'Warm hearted quilting--shaping up', url: "images/hearts.jpg"},
     {caption: 'The backbone of my warm-hearted quilt.', url: "images/heart-papers.png"},
-    {caption: 'Fabric inspiration', url: "images/fabric.png"}
+    {caption: 'Fabric inspiration', url: "images/fabric.png"},
+    {caption: 'My new life motto', url: "images/sloth.jpg"},
+    {caption: 'An ode to Yeezus', url: "images/kanye.jpg"},
+    {caption: 'First embroidery project.', url: "images/fun.jpg"}
 ];
 
 var container = document.querySelector('.image-list');
 
-for (var image of images) {
+var modal = document.getElementsByClassName('modal')[0];
+modal.style.display = 'none';
+
+var handleClick = function (event) {
+    var index = event.target.id;
+    var modalImage = document.getElementsByClassName('modal-image')[0];
+    modalImage.setAttribute('src', images[index].url);
+    modal.style.display = 'block';
+}
+
+for (var i = 0; i < images.length; i++) {
     var newImage = document.createElement('img');
-    newImage.setAttribute('src', image.url);
+    newImage.setAttribute('src', images[i].url);
     newImage.classList.add('image');
+    
 
     var caption = document.createElement('p');
-    caption.textContent = image.caption;
+    caption.textContent = images[i].caption;
     caption.classList.add('caption')
+    caption.id = i;
+    caption.onclick = handleClick;
 
     var listItem = document.createElement('li');
     listItem.classList.add('image-post');
@@ -30,3 +48,8 @@ for (var image of images) {
 
     container.appendChild(post);
 }
+
+
+
+
+
